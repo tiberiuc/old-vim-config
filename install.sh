28d70b569f821e64421e94b2c54506fa3c92ccb7
Tiberiu Craciun <tibi@happysoft.ro>
2013-02-19 22:31:31 +0200

    update to latest versions

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 git submodule init
-git submodule update
-git submodule foreach git checkout master
-git submodule foreach git pull
+git submodule update --merge --
+#git submodule foreach git checkout master
+#git submodule foreach git pull
 
 rm -rf ~/.vim
 mkdir -p ~/.vim
