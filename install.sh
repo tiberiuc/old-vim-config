a8657e1486c6fa5d649c3d818ab22a78303e73ee
Tibi Craciun <tibi@happysoft.ro>
2015-03-13 23:38:16 +0200

    Force git to use master on all submodules

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 git submodule init
 git submodule update --rebase --
 git submodule foreach git checkout master
-#git submodule foreach git pull
+#git submodule foreach git reset --hard origin/master
 
 rm -rf ~/.vim
 mkdir -p ~/.vim
